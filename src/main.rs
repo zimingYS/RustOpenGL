f8614ac6dfce5d58a6519caf4a3adf2af7395bc6
zimingYS <1563238589@qq.com>
2024-11-21 17:03:36 +0800

    修改着色器，使其能绘制出彩色三角形

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ use glium::Surface;
 #[derive(Copy, Clone)]
 struct Vertex {
     position: [f32; 2],
+    color: [f32; 3],
 }
-implement_vertex!(Vertex, position);
+implement_vertex!(Vertex, position, color);
 
 
 
@@ -20,9 +21,9 @@ fn main() {
     //绘制三角形坐标
     //更新顶点位置
     let shape = vec![
-        Vertex { position: [-0.5 , -0.5] },
-        Vertex { position: [ 0.0 ,  0.5] },
-        Vertex { position: [ 0.5 , -0.25] }
+        Vertex { position: [-0.5 , -0.5],  color: [1.0, 0.0, 0.0] },
+        Vertex { position: [ 0.0 ,  0.5],  color: [0.0, 1.0, 0.0] },
+        Vertex { position: [ 0.5 , -0.25], color: [0.0, 0.0, 1.0] }
     ];
     //顶点缓冲区
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
@@ -31,12 +32,13 @@ fn main() {
     //顶点着色器
     let vertex_shader_src = r#"
         #version 140
-
         in vec2 position;
-
+        in vec3 color;      // our new attribute
+        out vec3 vertex_color;
         uniform mat4 matrix;
 
         void main() {
+            vertex_color = color; // we need to set the value of each `out` variable.
             gl_Position = matrix * vec4(position, 0.0, 1.0);
         }
     "#;
@@ -44,9 +46,10 @@ fn main() {
     //片段着色器
     let fragment_shader_src = r#"
         #version 140
+        in vec3 vertex_color;
         out vec4 color;
         void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
+            color = vec4(vertex_color, 1.0);
         }
     "#;
 
@@ -70,11 +73,11 @@ fn main() {
                 //绘制图像
                 glium::winit::event::WindowEvent::RedrawRequested => {
                     // 把绘制代码放到这里！
-                    t += 0.2;
+                    t += 0.01;
                     let x = t.sin() * 0.5;
 
                     let mut target = display.draw();
-                    target.clear_color(0.0, 0.0, 1.0, 1.0);
+                    target.clear_color(0.0, 0.0, 0.0, 1.0);
 
                     let uniforms = uniform! {
                         matrix: [
